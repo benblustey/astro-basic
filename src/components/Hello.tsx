51ed53fb0f3b3e6ed24c8a3f42f671dede440fba
Benjamin Steyaert <appleholic@gmail.com>
2025-06-16 12:56:23 -0700

    Another attempt by passing to react component instead of JSON.

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,14 +1,15 @@
-import type { Event, HourlyEvent } from '../utils/eventTypes';
+import type { AllEventsData, HourlyEvent } from '../utils/eventTypes';
 
 // THIS IS THE ISSUE
 // need to get the data from the 
 // api during the build time
 
-const allEvents = await fetch('http://localhost:4321/api/allEventsLibs.json');
-const allEventsData = await allEvents.json();
-const hourEvents = allEventsData.hourlyEvents;
+// const allEvents = await fetch('http://localhost:4321/api/allEventsLibs.json');
+// const allEventsData = await allEvents.json();
+// const hourEvents = allEventsData.hourlyEvents;
 
-const Hello = () => {
+const Hello = (allEventsData: AllEventsData) => {
+  const hourEvents = allEventsData.hourlyEvents;
   return (
     <>
       <h2>Hello from React</h2>
