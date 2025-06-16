1f3e7eef25c60ce3ad4c4ca75edbc3e92c6c1441
Benjamin Steyaert <appleholic@gmail.com>
2025-06-16 10:29:28 -0700

    removed api calls

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,14 +1,18 @@
 import type { Event, HourlyEvent } from '../utils/eventTypes';
 
-const allEvents = await fetch('http://localhost:4321/api/events/allEventsData.json');
-const allEventsData = await allEvents.json();
-const hourEvents = allEventsData.hourlyEvents;
+// THIS IS THE ISSUE
+// need to get the data from the 
+// api during the build time
+
+// const allEvents = await fetch('http://localhost:4321/api/events/allEventsData.json');
+// const allEventsData = await allEvents.json();
+// const hourEvents = allEventsData.hourlyEvents;
 
 const Hello = () => {
   return (
     <>
       <h2>Hello from React</h2>
-      <h3>Total Events: {allEventsData.eventTotal}</h3>
+      {/* <h3>Total Events: {allEventsData.eventTotal}</h3>
       <h3>Total Days: {allEventsData.daysTotal}</h3>
       <div>
         {hourEvents.map((event: HourlyEvent) => (
@@ -16,7 +20,7 @@ const Hello = () => {
             <h2>{event.hour}: {event.count}</h2>
           </div>
         ))}
-      </div>
+      </div> */}
     </>
   );
 };
